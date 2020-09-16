f507274c70a75db1ff17dce105b0ac49f09f7996
oliviasun7 <ylisun@ucdavis.edu>
2020-09-16 22:09:56 +0800

    first commit

---
+++ src/components/Tourlist/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import Tour from "../Tour/Tour";
+import "./TourList.scss";
+import { tourData } from "./tourData";
+
+export default class TourList extends Component {
+  state = {
+    tours: tourData,
+  };
+  removeTour = (id) => {
+    const { tours } = this.state;
+    const filteredTours = tours.filter((tour) => tour.id != id);
+    this.setState({ tours: filteredTours });
+  };
+  render() {
+    const tourslist = this.state.tours.map((tour) => {
+      return <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />;
+    });
+    return <section className="tourlist">{tourslist}</section>;
+  }
+}
